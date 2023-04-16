ddc8f4f1f0d4130236b426912de2f2834c67b46a
vikas-grover <vikas.grover@yahoo.com>
2023-04-16 19:04:26 -0400

    local core cocepts

--- dags/core_concepts.py
+++ dags/core_concepts.py
@@ -1,6 +1,15 @@
 from airflow import DAG
 from airflow.utils.dates import days_ago
 
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+
+from airflow.utils.helpers import chain, cross_downstream
+
+from random import seed, random
+
+from datetime import timedelta
+
 default_arguments = {'owner': 'VG', 'start_date':days_ago(1)}
 
 with DAG(dag_id = 'Core_concepts', 
@@ -8,3 +17,17 @@ with DAG(dag_id = 'Core_concepts',
         catchup = False,
         default_args = default_arguments
         ) as dag:
+    bash_task = BashOperator(
+        task_id="bash_command",
+        bash_command="echo $TODAY",
+        env={"TODAY": "2020-05-21"}
+    )
+    def print_random_number(number):
+        seed(number)
+        print(random())
+
+    python_task = PythonOperator(
+        task_id="python_function", python_callable=print_random_number, op_args=[1],
+    )
+
+bash_task >> python_task
